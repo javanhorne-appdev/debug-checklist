a4bea1231b6abc5bc840680d6f0fd70474ee15ff
Jelani Woods <jelani@firstdraft.com>
2020-05-12 10:56:56 -0500

    Maybe starting point?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def show_checklist
+    render({ :template => "application/checklist.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get("/", { :controller => "application", :action => "show_checklist" })
+
+
+  # ======= Add your routes above here =============
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
